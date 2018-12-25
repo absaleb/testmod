a8f7d2b75c0af6b45d127e56e57f41b634885a25
abs <abs@okta-solutions.com>
2018-12-25 15:09:52 +0300

    correct output

--- testmod.go
+++ testmod.go
@@ -3,5 +3,5 @@ package testmod
 import "fmt"
 
 func Sum(a int, b int, name string) string {
-	return fmt.Sprintf("for %s sum is %d !!!", name, a+b)
+	return fmt.Sprintf("for %s sum of %d and %d is %d !!!", name, a, b, a+b)
 }
