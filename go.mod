29b0c2ad1e609355a764ac7a97e959f4fb02a73b
abs <abs@okta-solutions.com>
2018-12-25 16:15:19 +0300

    exif

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/salebgithub/testmod/v2
+
+require github.com/rwcarlsen/goexif v0.0.0-20180518182100-8d986c03457a // indirect
--- testmod.go
+++ testmod.go
@@ -1,7 +1,38 @@
 package testmod
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
+)
 
 func Sum(a int, b int, c int, name string) string {
 	return fmt.Sprintf("for %s sum of %d and %d and %d is %d !!!", name, a, b, c, a+b+c)
+
+}
+
+func GetExifDate(fname string) (*time.Time, error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	//camModel, _ := x.Get(exif.Model) // normally, don't ignore errors!
+
+	tm, err := x.DateTime()
+
+	return &tm, err
 }
